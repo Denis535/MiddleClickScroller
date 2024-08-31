9659cf45c4ec159ab687276450ebb0216d4e00d6
Denis <dddenisss35@gmail.com>
2024-08-31 17:36:08 +0300

    Initial commit

---
+++ MiddleClickScroller/MiddleClickScroller/MouseProcessor.cs
@@ -0,0 +1,59 @@
+﻿#nullable enable
+namespace MiddleClickScroller;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+using Microsoft.VisualStudio.Text.Editor;
+
+public class MouseProcessor : MouseProcessorBase {
+
+    private IWpfTextView View { get; }
+    private DispatcherTimer? Timer { get; set; }
+
+    public MouseProcessor(IWpfTextView view) {
+        View = view;
+        View.VisualElement.IsVisibleChanged += OnIsVisibleChanged;
+        View.Closed += OnClosed;
+    }
+
+    private void OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e) {
+        if ((bool) e.NewValue == false) {
+            Timer?.Stop();
+            Timer = null;
+        }
+    }
+
+    private void OnClosed(object sender, EventArgs e) {
+        Timer?.Stop();
+        Timer = null;
+        View.VisualElement.IsVisibleChanged -= OnIsVisibleChanged;
+        View.Closed -= OnClosed;
+    }
+
+    public override void PreprocessMouseDown(MouseButtonEventArgs e) {
+        if (e.ChangedButton == MouseButton.Middle) {
+            if (Timer == null) {
+                Timer = new DispatcherTimer(
+                    TimeSpan.FromMilliseconds( 20 ),
+                    DispatcherPriority.Normal,
+                    (sender, e) => {
+                        View.ViewScroller.ScrollViewportVerticallyByPixels( -5 );
+                    },
+                    View.VisualElement.Dispatcher );
+            }
+        }
+    }
+
+    public override void PreprocessMouseUp(MouseButtonEventArgs e) {
+        if (e.ChangedButton == MouseButton.Middle) {
+            Timer?.Stop();
+            Timer = null;
+        }
+    }
+
+}
---
+++ MiddleClickScroller/MiddleClickScroller/MouseProcessorFactory.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+namespace MiddleClickScroller;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+[Export( typeof( IMouseProcessorProvider ) )]
+[Name( "MiddleClickScroller" )]
+[Order( Before = "UrlClickMouseProcessor" )]
+[ContentType( "text" )]
+[TextViewRole( PredefinedTextViewRoles.Document )]
+[TextViewRole( PredefinedTextViewRoles.Editable )]
+public class MouseProcessorFactory : IMouseProcessorProvider {
+
+    public IMouseProcessor GetAssociatedProcessor(IWpfTextView view) {
+        return view.Properties.GetOrCreateSingletonProperty( () => new MouseProcessor( view ) );
+    }
+
+}
